7f1f7b0f82d8fdeccfc57458e3f6b5fb19e32524
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-03-12 18:40:47 +0200

    update 2 forgotten models

--- SupermarketsChain/SupermarketsChain.Models/Measure.cs
+++ SupermarketsChain/SupermarketsChain.Models/Measure.cs
@@ -1,6 +1,17 @@
 ﻿namespace SupermarketsChain.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Measure
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
     }
 }
