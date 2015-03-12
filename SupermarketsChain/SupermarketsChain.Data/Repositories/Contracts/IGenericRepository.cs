92c6e3ec1b469d3f6d2d53765be28ca824b61790
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-03-12 20:14:27 +0200

    add interface IGenericRepository and implementation of it - GenericRepository.cs

---
+++ SupermarketsChain/SupermarketsChain.Data/Repositories/Contracts/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace SupermarketsChain.Data.Repositories.Contracts
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    public interface IGenericRepository<T>
+    {
+        IQueryable<T> All();
+
+        IQueryable<T> Search(Expression<Func<T, bool>> conditions);
+
+        T GetById(object id);
+
+        void Add(T entity);
+
+        T Delete(T entity);
+
+        void Update(T entity);
+
+        T DeleteById(object id);
+
+        void SaveChanges();
+    }
+}
