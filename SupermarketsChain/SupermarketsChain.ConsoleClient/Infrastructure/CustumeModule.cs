0d774a5d369da227827ded75dd59f44281aa9be4
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-03-13 15:29:11 +0200

    add sample ObjectFactory for Ninject DI

---
+++ SupermarketsChain/SupermarketsChain.ConsoleClient/Infrastructure/CustumeModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketsChain.ConsoleClient.Infrastructure
+{
+    using Ninject.Modules;
+
+    class CustumeModule : INinjectModule
+    {
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void OnLoad(Ninject.IKernel kernel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnUnload(Ninject.IKernel kernel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnVerifyRequiredModules()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ninject.IKernel Kernel
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ SupermarketsChain/SupermarketsChain.ConsoleClient/Infrastructure/ObjectFactory.cs
@@ -0,0 +1,96 @@
+﻿namespace SupermarketsChain.ConsoleClient.Infrastructure
+{
+    using System;
+
+    using Ninject;
+    using Ninject.Modules;
+
+    using SupermarketsChain.Data;
+
+    public static class ObjectFactory
+    {
+        private static readonly IKernel m_kernel = new StandardKernel();
+
+        /// <summary>
+        /// Static accessor to the Ninject kernel
+        /// </summary>
+        public static IKernel Kernel
+        {
+            get { return m_kernel; }
+        }
+
+        /// <summary>
+        /// Gets an object using Ninject
+        /// </summary>
+        /// <typeparam name="T">The type of object to get</typeparam>
+        /// <returns>The object if successful, throws an exception if failed</returns>
+        public static T Get<T>()
+        {
+            return Kernel.Get<T>();
+        }
+
+        /// <summary>
+        /// Gets an object using Ninject
+        /// </summary>
+        /// <typeparam name="T">The type of object to return</typeparam>
+        /// <param name="type">The type of the object to get</param>
+        /// <returns>The object if successful, throws an exception if failed</returns>
+        public static T Get<T>(Type type)
+        {
+            return (T)Kernel.Get(type);
+        }
+
+        /// <summary>
+        /// Gets a named object using Ninject
+        /// </summary>
+        /// <typeparam name="T">The type of object to get</typeparam>
+        /// <param name="name">The name of the object</param>
+        /// <returns>The object if successful, throws an exception if failed</returns>
+        public static T Get<T>(string name)
+        {
+            return Kernel.Get<T>(name);
+        }
+
+        /// <summary>
+        /// Trys to get an object using Ninject
+        /// </summary>
+        /// <typeparam name="T">The type of object to get</typeparam>
+        /// <returns>The object if successful, null if failed</returns>
+        public static T TryGet<T>()
+        {
+            return Kernel.TryGet<T>();
+        }
+
+        /// <summary>
+        /// Trys to get an object using Ninject
+        /// </summary>
+        /// <typeparam name="T">The type of object to return</typeparam>
+        /// <param name="type">The type of the object to get</param>
+        /// <returns>The object if successful, throws an exception if failed</returns>
+        public static T TryGet<T>(Type type)
+        {
+            return (T)Kernel.TryGet(type);
+        }
+
+        /// <summary>
+        /// Trys to get an object using Ninject
+        /// </summary>
+        /// <typeparam name="T">The type of object to get</typeparam>
+        /// <param name="name">The name of the object</param>
+        /// <returns>The object if successful, null if failed</returns>
+        public static T TryGet<T>(string name)
+        {
+            return Kernel.TryGet<T>(name);
+        }
+
+        /// <summary>
+        /// Load a new Ninject dependency module into the kernel
+        /// </summary>
+        /// <param name="module">The module to load</param>
+        public static void Load(INinjectModule module)
+        {
+            Kernel.Bind<ISupermarketsChainData>().To<SupermarketsChainData>();
+            Kernel.Load(module);
+        }
+    }
+}
