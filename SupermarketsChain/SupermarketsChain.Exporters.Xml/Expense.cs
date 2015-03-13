34b938cdbecc8c5eadfc4f6272d0b9704b4a4119
kyamaliev <kyamaliev@gmail.com>
2015-03-14 01:54:38 +0200

    Add console client for the XML exporter which generates the desired output. Cannot test it in this solution because App.config file is missing. Will refactor it to create a method which can be called from arbitrary location.

---
+++ SupermarketsChain/SupermarketsChain.Exporters.Xml/Expense.cs
@@ -0,0 +1,17 @@
+﻿namespace XmlExporterConsoleClient
+{
+    using System;
+
+    public class Expense
+    {
+        public DateTime Date { get; set; }
+
+        public decimal ExpenseValue { get; set; }
+
+        public Expense(DateTime date, decimal expenseValue)
+        {
+            this.Date = date;
+            this.ExpenseValue = expenseValue;
+        }
+    }
+}
---
+++ SupermarketsChain/SupermarketsChain.Exporters.Xml/Vendor.cs
@@ -0,0 +1,33 @@
+﻿namespace XmlExporterConsoleClient
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Vendor
+    {
+        private readonly List<Expense> expenses = new List<Expense>(); 
+        
+        public string Name { get; set; }
+
+        public Vendor(string name)
+        {
+            this.Name = name;
+        }
+
+        public IEnumerable<Expense> Expenses
+        {
+            get { return this.expenses; }
+        }
+
+        public Vendor WithExpenses(DateTime date, decimal amount)
+        {
+            this.AddExpense(new Expense(date, amount));
+            return this;
+        }
+
+        private void AddExpense(Expense expense)
+        {
+            this.expenses.Add(expense);
+        }
+    }
+}
