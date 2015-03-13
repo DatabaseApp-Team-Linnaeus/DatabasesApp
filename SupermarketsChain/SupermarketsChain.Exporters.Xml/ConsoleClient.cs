eba18ccb1c78031979ac37ad9a0bd646f2b26ba7
kyamaliev <kyamaliev@gmail.com>
2015-03-14 01:21:09 +0200

    add sample data for to generate the XML from

---
+++ SupermarketsChain/SupermarketsChain.Exporters.Xml/ConsoleClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketsChain.Exporters.Xml
+{
+    class ConsoleClient
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
