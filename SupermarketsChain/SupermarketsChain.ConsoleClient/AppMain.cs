685645dc7f90c6ff4dcc4f174ae872b2dcdb8d58
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-03-13 15:28:06 +0200

    add new console app to test models

---
+++ SupermarketsChain/SupermarketsChain.ConsoleClient/AppMain.cs
@@ -0,0 +1,18 @@
+﻿namespace SupermarketsChain.ConsoleClient
+{
+    using SupermarketsChain.Data;
+    using SupermarketsChain.Models;
+
+    public class AppMain
+    {
+        public static void Main(string[] args)
+        {
+            var context = new SupermarketsChainDbContext();
+            // this throws exeption - use only context
+            //var data = new SupermarketsChainData(context);
+            context.Towns.Add(new Town() { Name = "Vraca" });
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ SupermarketsChain/SupermarketsChain.ConsoleClient/SampleTestMethod.cs
@@ -0,0 +1,22 @@
+﻿namespace SupermarketsChain.ConsoleClient
+{
+    using SupermarketsChain.ConsoleClient.Infrastructure;
+    using SupermarketsChain.Data;
+    using SupermarketsChain.Models;
+
+    public class SampleTestMethod
+    {
+        private ISupermarketsChainData data;
+
+        public SampleTestMethod()
+        {
+            this.data = ObjectFactory.Get<ISupermarketsChainData>();
+        }
+
+        public void CreateEntities()
+        {
+            var measure = new Measure() { Name = "kilogram" };
+            this.data.Measures.Add(measure);
+        }
+}
+}
