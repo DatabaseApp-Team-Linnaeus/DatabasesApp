21a1e61e389242dbfc95d51c6de37c26c1bd7729
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-03-14 17:25:15 +0200

    Json.Net is the best libs for our needs. Commit a simple export class that export array in json file in D://   resolve #15

---
+++ SupermarketsChain/SupermarketsChain.ReportToJson/JsonTest.cs
@@ -0,0 +1,25 @@
+﻿namespace SupermarketsChain.ReportToJson
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    public class JsonTest
+    {
+        public static void Main()
+        {
+            List<List<string>> _data = new List<List<string>>();
+            _data.Add(new List<string>()
+            {
+                "dsadsa", "dsdsa", "dsassdas"
+            });
+            string json = JsonConvert.SerializeObject(_data.ToArray());
+
+            //write string to file
+            System.IO.File.WriteAllText(@"D:\path.json", json);
+            Console.WriteLine("yes");
+            Console.ReadLine();
+        }
+    }
+}
