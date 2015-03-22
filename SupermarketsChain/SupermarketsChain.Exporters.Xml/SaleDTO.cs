a07d5468838803aa21ff8fa47f8d8e24799c85e4
Konstantin Yamaliev <kyamaliev@gmail.com>
2015-03-22 21:46:03 +0200

    Closed #4

---
+++ SupermarketsChain/SupermarketsChain.Exporters.Xml/SaleDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace XmlExporterConsoleClient
+{
+    using System;
+
+    public class SaleDto
+    {
+        public SaleDto(DateTime date, decimal saleValue)
+        {
+            this.Date = date;
+            this.SaleValue = saleValue;
+        }
+
+        public DateTime Date { get; set; }
+
+        public decimal SaleValue { get; set; }
+    }
+}
